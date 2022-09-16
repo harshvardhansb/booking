18427ad6e4067c2d3f7cf1ff6f7541ada644bcbe
Harsh Vardhan Singh <95876759+harshvardhansb@users.noreply.github.com>
2022-09-16 19:08:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -56,19 +56,7 @@ function Card(props) {
 
 
 
-/*
 
-<Card
-      img="https://images.unsplash.com/photo-1592947945242-69312358628b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80"
-      title="Tie Up Boots"
-      description="Fall Favorite • Boots"
-      speciality="Type-4"
-    />
-    
-    
-
-
-*/
 
 
 
